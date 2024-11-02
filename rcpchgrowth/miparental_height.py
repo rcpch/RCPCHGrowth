af4c0436f6455bdc5229d2c043a83fe9ce74a703
eatyourpeas <simon@eatyourpeas.co.uk>
2024-11-02 09:47:33 +0000

    correct mph, add lower upper thresholds

--- rcpchgrowth/miparental_height.py
+++ rcpchgrowth/miparental_height.py
@@ -8,11 +8,14 @@ Tanner JM, Whitehouse RH, Takaishi M. Standards from birth to maturity for heigh
 The strengths and limitations of parental heights as a predictor of attained height, Charlotte M Wright, Tim D Cheetham, Arch Dis Child 1999;81:257–260
 """
 
-def mid_parental_height(maternal_height, paternal_height):
+def mid_parental_height(maternal_height, paternal_height, sex):
     """
     Calculate mid-parental height
     """
-    return (maternal_height + paternal_height + 13) / 2
+    if sex == MALE:
+        return (maternal_height + paternal_height + 13) / 2
+    else:
+        return (maternal_height + paternal_height - 13) / 2
 
 def mid_parental_height_z(maternal_height, paternal_height, reference=UK_WHO):
     """
@@ -37,3 +40,11 @@ def expected_height_z_from_mid_parental_height_z(mid_parental_height_z):
     """
     
     return mid_parental_height_z * 0.5
+
+def lower_and_upper_limits_of_expected_height_z(mid_parental_height_z):
+    """
+    Calculate lower and upper limits of expected height z score from mid-parental height z-score
+    Returns a tuple of (lower, upper) limits
+    """
+    
+    return mid_parental_height_z - 1.4, mid_parental_height_z + 1.4
