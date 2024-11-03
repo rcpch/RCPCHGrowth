8a9dd41b074b271e6a7edfe5ab00e4e94c17fef4
eatyourpeas <simon@eatyourpeas.co.uk>
2024-11-03 19:38:42 +0000

    remove validation constants

--- rcpchgrowth/constants/validation_constants.py
+++ rcpchgrowth/constants/validation_constants.py
@@ -1,11 +1,12 @@
 """
 These constants are intended to be used throughout both the rcpchgrowth module
 and the API for validation of input values and rejection of very high or low values
+
+The constants used previously drew on the Guiness Book of Records and other sources to set hard limits.
+These were replaced with SDS sds_advisory and sds_error values in the rcpchgrowth-python repository as per issue #32
 """
 
 # HEIGHT CONSTANTS
-MINIMUM_LENGTH_CM = 20
-MAXIMUM_HEIGHT_CM = 300
 # The smallest baby was report by the BBC on 9/8/2021 (https://www.bbc.co.uk/news/world-asia-58141756.amp)
 # to have a length of just 24cm and was born at 25 weeks gestation, discharged at 13 mths of age (chronological)
 # from Singapore's National University Hospital
@@ -14,8 +15,6 @@ MAXIMUM_HEIGHT_CM = 300
 # https://en.wikipedia.org/wiki/Robert_Wadlow
 
 # WEIGHT CONSTANTS
-MINIMUM_WEIGHT_KG = 0.15
-MAXIMUM_WEIGHT_KG = 500
 # The smallest baby was report by the BBC on 9/8/2021 (https://www.bbc.co.uk/news/world-asia-58141756.amp)
 # to weigh just 212g and was born at 25 weeks gestation, discharged at 13 mths of age (chronological)
 # from Singapore's National University Hospital
@@ -25,16 +24,12 @@ MAXIMUM_WEIGHT_KG = 500
 # https://en.wikipedia.org/wiki/Jon_Brower_Minnoch
 
 # OFC CONSTANTS
-MINIMUM_OFC_CM = 5
-MAXIMUM_OFC_CM = 100
 # We couldn't find a good answer for the 'largest human head' that we could use
 # Instead have presumed that 100 is a good maximum, given that pathological states
 # causing very large head size (eg hydrocephalus) render the OFC meaningless for
 # growth measurement and assessment purposes.
 
 # BMI CONSTANTS
-MINIMUM_BMI_KGM2 = 7.5
-MAXIMUM_BMI_KGM2 = 105
 # Again, John Brower Minnoch comes to our rescue here. HIs body mass index
 # at the time of his death was 105 kg/m2. In terms of the lowest body mass index ever recorded
 # there was nly limited web search information that suggested 7.5 kg.m2 as the lowest ever.
